06b1522dd75db3d49d3d9ee94c297707b18d3f5b
cnc-kvsr <kevsercanca@gmail.com>
2021-01-21 21:31:43 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<T,F>
+    {
+        T[] id;
+        F[] firstName;
+
+        public MyDictionary()
+        {
+            id = new T[0];
+            firstName = new F[0];
+        }
+
+        public void Add(T _id,F _firstName)
+        {
+            T[] tempArray1 = id;
+            id = new T[id.Length + 1];
+
+            F[] tempArray2 = firstName;
+            firstName = new F[firstName.Length + 1];
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                id[i] = tempArray1[i];
+                firstName[i] = tempArray2[i];
+            }
+            id[id.Length - 1] = _id;
+            firstName[firstName.Length - 1] = _firstName;
+
+        }
+        public T[] Id
+        {
+            get { return id; }
+
+        }
+        public F[] FirstName
+        {
+            get { return firstName; }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> customers = new MyDictionary<int, string>();
+            customers.Add(1, "Kevser");
+            customers.Add(2, "Nagehan");
+            customers.Add(3, "Kağan");
+
+            foreach (var item in customers.Id)
+            {
+                Console.WriteLine($"Customer Id: {item}");
+
+            }
+            foreach (var item2 in customers.FirstName)
+            {
+                Console.WriteLine($"Customer Name: {item2}");
+            }
+           
+           
+        }
+    }
+}
